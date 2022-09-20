e01340692709493269496470ee1c228eb9bef224
Gentito <tsietsiramosedi6@gmail.com>
2022-09-20 15:03:31 -0700

    101 fix

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n < 0:
-        return str
-    count = 0
-    str_dup = ""
-    for elem in str:
-        if count == n:
-            count += 1
-            continue
-        str_dup += str[count]
-        count += 1
-        return str_dup
+    i = str[:n]
+    x = str[n+1:]
+    return i + x
