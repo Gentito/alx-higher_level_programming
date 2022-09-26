f6ad5967244850f37f9fae4d808a92893c50456a
Gentito <tsietsiramosedi6@gmail.com>
2022-09-26 14:25:33 -0700

    fix all

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """
-    A function that returns a tuple with 
+    A function that returns a tuple with
     the length of a string and its first character.
     """
     if sentence == "":
